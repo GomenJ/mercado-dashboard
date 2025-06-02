98d5a91ddb13008977f8aecbba4cf25845a3bbc7
GomenJ <juan.gonzalez.mendoza.j@gmail.com>
2025-06-01 21:21:39 -0600

    chore: add initial route for pml-pnd-screen

--- src/routes/pml-mda/index.tsx
+++ src/routes/pml-mda/index.tsx
@@ -1,10 +1,32 @@
+import { pmlAnualQueryOptions } from "@/feautures/pml/api/pml-anual-query-options";
+import { pmlDiarioQueryOptions } from "@/feautures/pml/api/pml-diario-query-options";
 import { pndQueryOptions } from "@/feautures/pml/api/pnd-query-options";
 import { createFileRoute } from "@tanstack/react-router";
-// import { useEffect } from "react";
+import { z } from "zod";
+
+export const SIN_GERENCIAS = [
+	"CENTRAL",
+	"NORESTE",
+	"NOROESTE",
+	"NORTE",
+	"OCCIDENTAL",
+	"ORIENTAL",
+	"PENINSULAR",
+] as const;
+
+
+const pmlMdaSchema = z.object({
+	gerencia: z.enum(SIN_GERENCIAS).optional()
+})
 
 export const Route = createFileRoute("/pml-mda/")({
+	validateSearch: pmlMdaSchema,
 	loader: ({ context: { queryClient } }) => {
-		return queryClient.ensureQueryData(pndQueryOptions)
+		return Promise.all([
+			queryClient.ensureQueryData(pmlAnualQueryOptions),
+			queryClient.ensureQueryData(pndQueryOptions),
+			queryClient.ensureQueryData(pmlDiarioQueryOptions)
+		]);
 	},
 });
 
